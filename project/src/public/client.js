4a71c3a9225f87f4bbe0b95887d87afb32aec378
John Jordan <johnjordan1985@gmail.com>
2020-12-27 23:05:31 -0800

    refactor: remove template/sample code, and set first rover isntance as active rover

--- project/src/public/client.js
+++ project/src/public/client.js
@@ -19,37 +19,15 @@ const updateStore = (store, newState) => {
   render(root, newStore);
 };
 
-// create content
-// const App = (state) => {
-//   const { rovers, apod } = state;
-
-//   return `
-//         <header></header>
-//         <main>
-//             ${Greeting(store.user.name)}
-//             <section>
-//                 <h3>Put things on the page!</h3>
-//                 <p>Here is an example section.</p>
-//                 <p>
-//                     One of the most popular websites at NASA is the Astronomy Picture of the Day. In fact, this website is one of
-//                     the most popular websites across all federal agencies. It has the popular appeal of a Justin Bieber video.
-//                     This endpoint structures the APOD imagery and associated metadata so that it can be repurposed for other
-//                     applications. In addition, if the concept_tags parameter is set to True, then keywords derived from the image
-//                     explanation are returned. These keywords could be used as auto-generated hashtags for twitter or instagram feeds;
-//                     but generally help with discoverability of relevant imagery.
-//                 </p>
-//                 ${ImageOfTheDay(apod)}
-//             </section>
-//         </main>
-//         <footer></footer>
-//     `;
-// };
 const isActive = (rover, state) => {
   if (rover === state.activeRover) {
     return "active";
   }
   return "";
 };
+
+
+// ------------------------------------------------------  COMPONENTS
 
 const buildNavList = (roverList, state) => roverList
   .map(
@@ -113,34 +91,6 @@ window.addEventListener("load", () => {
   render(root, store);
 });
 
-// ------------------------------------------------------  COMPONENTS
-
-// Example of a pure function that renders infomation requested from the backend
-const ImageOfTheDay = (apod) => {
-  // If image does not already exist, or it is not from today -- request it again
-  const today = new Date();
-  const photodate = new Date(apod.date);
-  console.log(photodate.getDate(), today.getDate());
-
-  console.log(photodate.getDate() === today.getDate());
-  if (!apod || apod.date === today.getDate()) {
-    getImageOfTheDay(store);
-  }
-
-  // check if the photo of the day is actually type video!
-  if (apod.media_type === "video") {
-    return `
-            <p>See today's featured video <a href="${apod.url}">here</a></p>
-            <p>${apod.title}</p>
-            <p>${apod.explanation}</p>
-        `;
-  }
-  return `
-            <img src="${apod.image.url}" height="350px" width="100%" />
-            <p>${apod.image.explanation}</p>
-        `;
-};
-
 // ------------------------------------------------------  API CALLS
 
 // Example API call
@@ -178,7 +128,7 @@ const getRoverData = (roverName, state) => {
       };
       // use immutableJS here
       allRoversData.push(rover);
-      updateStore(store, { allRoversData, activeRover: "Spirit" });
+      updateStore(store, { allRoversData });
     });
 
   // return data;
@@ -187,6 +137,8 @@ const getRoverData = (roverName, state) => {
 store.rovers.forEach((rover) => {
   getRoverData(rover, store);
 });
+
+updateStore(store, { activeRover: store.rovers[0] });
 
 const isTabButtonClicked = (elemId) => elemId.includes("tab_toggle");
 
